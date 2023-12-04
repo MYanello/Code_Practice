91f8e8e24957a0611662df8f2a46bc37e51c1471
myanello-rescale <myanello@rescale.com>
2023-12-04 14:03:19 -0600

    bit of an ugly scoring mech but part 1 done

--- 4/main.py
+++ 4/main.py
@@ -3,11 +3,44 @@ from icecream import ic
 import regex as re
 
 def lotto(file):
-    score = 0
+    win_nums = []
+    my_nums = []
+    with open(file) as f:
+        lines = f.readlines()
+        score = 0
+        for line in lines:
+            matches = 0
+            card_score = 0
+            #ic(line)
+            card = re.split(r'\:|\|',line)
+            #ic(card)
+            win_nums = card[1].strip()
+            win_nums = re.split(r'\ ',win_nums)
+            win_nums = list(filter(None, win_nums))
+            my_nums = card[2].strip()
+            my_nums = re.split(r'\ ',my_nums)
+            my_nums = list(filter(None, my_nums))
+            ic(win_nums)
+            ic(my_nums)
+            for num in my_nums:
+                if num in win_nums:
+                    matches += 1
+            ic(matches)
+            if matches == 0:
+                card_score = 0
+            elif matches == 1:
+                card_score = 1
+            else:
+                card_score = 1
+                while matches > 1:
+                    card_score *= 2
+                    matches -= 1
+            ic(card_score)
+            score += card_score
+            ic(score)
     return score
 
 if __name__ == '__main__':
-    #assert lotto('test.txt') == 13
+    assert lotto('test.txt') == 13
     print(lotto('test.txt'))
-    #ic.disable()
-    #print(parts1('input.txt'))
+    print(lotto('input.txt'))
