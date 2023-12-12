de0dadc652279474917672dd0ad5b489fbe841b3
Marcus <marcus@yanello.net>
2023-12-12 12:00:43 -0600

    close ish

--- 12/main.py
+++ 12/main.py
@@ -3,15 +3,31 @@ from icecream import ic
 import numpy as np
 import regex as re
 
+class row:
+    def __init__(self, data):
+        self.mapp = data[0]
+        self.counts = (data[1].split(','))
+        self.counts = [int(count) for count in self.counts]
+        ic(self.mapp, self.counts)
+        self.grouping = [x for x in re.split(r'\.+', self.mapp) if x]
+        ic(self.grouping)
+
+    def get_poss(self):
+        poss = 0
+        for i, set in enumerate(self.grouping):
+            if '?' in set:
+                poss += int(self.counts[i]) % set.count('?')
+                ic(set.count('?'), self.counts[i], set)
+                ic(poss)
+
 def parser(file):
     with open(file, 'r') as f:
         data = [line.strip() for line in f.readlines()]
         data = [re.split(r' ',line) for line in data]
-        maps = [line[0] for line in data]
-        counts = [line[1] for line in data]
-    return maps, counts
+    return data
 
 if __name__ == '__main__':
-    maps, counts = parser('test.txt')
-    ic(maps, counts)
-    
+    data = parser('test.txt')
+    ic(data[1])
+    row1 = row(data[1])
+    row1.get_poss()
