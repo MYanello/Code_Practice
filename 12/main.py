b4061bec826528da7391b9f65d1a4af819edb51d
Marcus <marcus@yanello.net>
2023-12-12 09:29:17 -0600

    comprehensions and parsers

---
+++ 12/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from icecream import ic
+import numpy as np
+import regex as re
+
+def parser(file):
+    with open(file, 'r') as f:
+        data = f.readlines()
+        data = [line.strip() for line in data]
+        data = [re.split(r' ',line) for line in data]
+        maps = [line[0] for line in data]
+        counts = [line[1] for line in data]
+    return maps, counts
+
+if __name__ == '__main__':
+    maps, counts = parser('test.txt')
+    ic(maps, counts)
+    
